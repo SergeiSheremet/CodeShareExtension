1ff59713e9c30cbfa0166ec2c5cee75995e96487
Unknown <sheremet5555@gmail.com>
2018-12-23 02:41:11 +0300

    Choose receiver

---
+++ CodeShare/FriendChoose.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VkLibrary.Core.Types.Users;
+
+namespace CodeShare
+{
+    /// <summary>
+    /// Interaction logic for FriendChoose.xaml
+    /// </summary>
+    public partial class FriendChoose : Window
+    {
+        public int? SelectedId { get; private set; }
+
+        public FriendChoose()
+        {
+            InitializeComponent();
+        }
+
+        private void Send(object sender, RoutedEventArgs e)
+        {
+            if (ListBox.SelectedItems.Count > 0)
+            {
+                var user = ListBox.SelectedItem as UserFull;
+                SelectedId = user?.Id;
+
+                Close();
+            }
+        }
+    }
+}
--- CodeShare/SocialNetwork/Implementation/Vk.cs
+++ CodeShare/SocialNetwork/Implementation/Vk.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EnvDTE;
 using VkLibrary.Core;
 using VkLibrary.Core.Auth;
 
@@ -11,12 +12,12 @@ namespace CodeShare.SocialNetwork.Implementation
     class Vk : ISocialNetwork
     {
         private readonly Vkontakte _vkLibrary;
-        private const string _accessTokenString = 
+        private const string AccessToken = 
             "ee424c4bcc0208b1cdf71ca5d5def9749d302e42da78e6ba44b04c2f355a2e927ea83afd4c0c35831adde";
 
         public Vk()
         {
-            _vkLibrary = new Vkontakte(6793423) {AccessToken = AccessToken.FromString(_accessTokenString)};
+            _vkLibrary = new Vkontakte(6793423, apiVersion: "5.80") {AccessToken = VkLibrary.Core.Auth.AccessToken.FromString(AccessToken)};
         }
 
         public void LogIn(string login, string password)
@@ -31,7 +32,11 @@ namespace CodeShare.SocialNetwork.Implementation
 
         public async void SendUrl(string url)
         {
-            var ok = await _vkLibrary.Messages.Send(userId: 142477265, message: url);
+            var friends = await _vkLibrary.Friends.Get();
+            FriendChoose choiceWindow = new FriendChoose {ListBox = {ItemsSource = friends.Items.OrderBy(x => x.FirstName)}};
+            choiceWindow.ShowDialog();
+            
+            var ok = await _vkLibrary.Messages.Send(userId: choiceWindow.SelectedId, message: url);
         }
     }
 }
